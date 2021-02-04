aca798d640c94b3706f13e1e93c0b9021d864cc3
Jaime Reuss <jamesreuss1@gmail.com>
2021-02-04 01:43:43 +0000

    added select queries

---
+++ 1_queries/05_allmyres.sql
@@ -0,0 +1,9 @@
+SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN reservations ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < NOW()::DATE
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date DESC
+LIMIT 10;
